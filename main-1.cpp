2eee6cb668d1f99fea6bf7d316aea3f8ab6b69d6
sumaiya-akter <sumiaya.akter.shampa@gmail.com>
2022-09-22 17:48:40 +0600

    Rewrite main.cpp code

---
+++ main-1.cpp
@@ -0,0 +1,59 @@
+
+#include <graphics.h>
+int main()
+{
+
+ int gd = DETECT, gm;
+ initgraph(&gd, &gm, "");
+ line(200, 70, 400, 70);//1st start line or row
+ line(100, 150, 500, 150);// 2nd start line or row
+ line(100, 400, 500, 400);// 3rd start line or row
+ line(200, 150, 200, 400);//left middle coloum
+ line(400, 150, 400, 400);//right middle coloum
+ line(200, 70, 100, 150);
+ line (200, 70,300, 150);
+ line(400, 70, 500, 150);
+ line(100, 150, 100, 400);//left coloum
+ line(500, 150, 500, 400);//right coloum
+ line(120,220,180,220);// left window-//row 1
+ line(120,330,180,330);//row 3
+ line(120,275,180,275);//row 2
+ line(120,220,120,330);//coloum 1
+ line(180,220,180,330);//coloum 3
+ line(150,220,150,330);//coloum 2
+ line(420,220,480,220);//right window -row 1
+ line(420,330,480,330);//row 2
+ line(420,275,480,275);//row 3
+ line(420,220,420,330);//coloum 1
+ line(480,220,480,330);//coloum 3
+ line(450,220,450,330);//coloum 2
+ // door
+ line(300,150,300,400);//middle coloum of door
+ line(220,180,280,190);//left side of door
+ line(220,370,280,360);
+ line(220,180,220,370);
+ line(280,190,280,360);
+ //right side of door
+ line(320,190,380,180);
+ line(320,360,380,370);
+ line(320,190,320,360);
+ line(380,180,380,370);
+ // for tree
+  line(30,80,30,420);
+  line(90,80,90,420);
+  //root
+  line(30,420,20,450);
+  line(90,420,100,450);
+  line(20,450,40,420);
+  line(40,420,50,450);
+  line(50,450,70,420);
+  line(70,420,100,450);
+  line(30,80,10,60);
+  line(30,60,50,80);
+  line(50,80,50,60);
+  line(70,80,70,60);
+  line(90,80,110,60);
+  line(70,80,90,60);
+ getch();
+ closegraph();
+}
--- main.cpp
+++ main.cpp
@@ -1,65 +1,66 @@
-
-
-#include <stdlib.h>
-
-
-
-
 #include <graphics.h>
 int main()
 {
 
  int gd = DETECT, gm;
  initgraph(&gd, &gm, "");
- line(200, 70, 400, 70);//1st start line or row
- line(100, 150, 500, 150);// 2nd start line or row
- line(100, 400, 500, 400);// 3rd start line or row
- line(200, 150, 200, 400);//left middle coloum
- line(400, 150, 400, 400);//right middle coloum
- line(200, 70, 100, 150);
- line (200, 70,300, 150);
- line(400, 70, 500, 150);
- line(100, 150, 100, 400);//left coloum
- line(500, 150, 500, 400);//right coloum
- line(120,220,180,220);// left window-//row 1
- line(120,330,180,330);//row 3
- line(120,275,180,275);//row 2
- line(120,220,120,330);//coloum 1
- line(180,220,180,330);//coloum 3
- line(150,220,150,330);//coloum 2
- line(420,220,480,220);//right window -row 1
- line(420,330,480,330);//row 2
- line(420,275,480,275);//row 3
- line(420,220,420,330);//coloum 1
- line(480,220,480,330);//coloum 3
- line(450,220,450,330);//coloum 2
- // door
- line(300,150,300,400);//middle coloum of door
- line(220,180,280,190);//left side of door
- line(220,370,280,360);
- line(220,180,220,370);
- line(280,190,280,360);
- //right side of door
- line(320,190,380,180);
- line(320,360,380,370);
- line(320,190,320,360);
- line(380,180,380,370);
- // for tree
-  line(30,80,30,420);
-  line(90,80,90,420);
-  //root
-  line(30,420,20,450);
-  line(90,420,100,450);
-  line(20,450,40,420);
-  line(40,420,50,450);
-  line(50,450,70,420);
-  line(70,420,100,450);
-  line(30,80,10,60);
-  line(30,60,50,80);
-  line(50,80,50,60);
-  line(70,80,70,60);
-  line(90,80,110,60);
-  line(70,80,90,60);
- getch();
+ initwindow(2000, 1000); // window size
+
+ // for first house
+ line(300,220,500,220);//first upper row of house
+ line(250,300,450,300);// Second row of the house
+ // coloum
+ line(300,220,250,300);//left side coloum
+ line(500,220,450,300);//left middle coloum
+ //right  side coloum
+ line(500,220,550,300);//right side coloum
+ line(490,240,530,300);//right middle coloum
+ line(530,300,550,300);// small row of house
+ //baricate
+ line(280,300,280,350);// baricate coloum left side
+ line(530,300,530,350);// baricate coloum right side
+ line(430,300,430,350);// baricate coloum middle
+ // base
+ line(280,350,530,350);// base or ground row of the house(Min)
+ line(270,360,540,360);// base or ground row of the house(Max)
+ // 3 samall coloum of house base
+ line(280,350,270,360);// right side
+ line(530,350,540,360);//left side
+ line(430,350,440,360);//Middle side
+ // for Second house
+ line(100,400,300,400);//first upper row of house
+ line(50,480,250,480);// Second row of the house
+  // coloum
+ line(100,400,50,480);//left side coloum
+ line(300,400,250,480);//left middle coloum
+ //right  side coloum
+ line(300,400,350,480);//right side coloum
+ line(290,420,330,480);//right middle coloum
+ line(350,480,330,480);// small row of house2
+ // baricate of house 2
+ line(70,480,70,530);// baricate coloum left side
+ line(330,480,330,530);// baricate coloum right side
+ line(230,480,230,530);//baricate coloum middle
+ //base
+ line(70,530,330,530);// base or ground row of the house(Min)
+ line(60,540,340,540);// base or ground row of the house(Max)
+ // 3 samall coloum of house base
+ line(330,530,340,540);// right side
+ line(70,530,60,540);//left side
+ line(230,530,240,540);//Middle side
+ // tree create body
+ line(110,400,110,350);// tree body right side
+ line(130,400,130,350);// tree body left side
+ // tree stalks
+ line(110,350,90,330);// right side
+ line(130,350,160,330);//left side
+ line(117,350,97,330);
+ line(123,350,153,330);
+ line(117,350,117,330);
+ line(123,350,123,330);
+ // tree stalks end
+
+
+  getch();
  closegraph();
 }
